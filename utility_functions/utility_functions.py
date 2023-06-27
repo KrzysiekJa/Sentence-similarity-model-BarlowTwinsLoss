05dd494e9a1dd88aef46c865553b5909bd772783
KrzysiekJa <kjmj77@gmail.com>
2023-06-28 01:05:12 +0200

    Modifications in scripts #12

--- utility_functions/utility_functions.py
+++ utility_functions/utility_functions.py
@@ -63,7 +63,7 @@ def neptun_final_steps(run, test_evaluation, language: str, model_save_path: str
     )
     
     run_pandas_df = project_read_only.fetch_runs_table(tag=["similarity", language]).to_pandas()
-    best_testing_result = run_pandas_df["test/test_accuracy"].iloc[0]
+    best_testing_result = run_pandas_df["test/test_accuracy"].iloc[0] if "test/test_accuracy" in run_pandas_df else -1
     
     if test_evaluation > best_testing_result:
         run["model/model"].upload(model_save_path)
